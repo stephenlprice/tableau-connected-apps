09f2945f55805a148c784f44c71c28ad29280d41
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-07-21 22:18:20 -0500

    moved env_vars to index for ease of use and avoid burying it in utils

--- python/index.py
+++ python/index.py
@@ -3,13 +3,23 @@ from dotenv import load_dotenv
 from utils import exceptions, log, environment
 from modules import connected_apps
 
+# dictionary with required environment variables
+env_vars = [
+  "TABLEAU_SERVER",
+  "TABLEAU_SITENAME",
+  "TABLEAU_USERNAME",
+  "TABLEAU_CA_CLIENT",
+  "TABLEAU_CA_SECRET_ID",
+  "TABLEAU_CA_SECRET_VALUE"
+]
+
 # load environment files from .env
 load_dotenv("../.env")
 # calling environ is expensive, this saves environment variables to a dictionary
 env_dict = dict(os.environ)
 
 # validate environment variables
-environment.validate(env_dict)
+environment.validate(env_dict, env_vars)
 
 # encode a JWT token for connected apps authentication: https://help.tableau.com/current/online/en-us/connected_apps.htm#step-4-embedding-next-steps
 connected_apps.encode(env_dict)
