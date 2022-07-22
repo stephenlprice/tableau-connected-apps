c624ea8c9b20be9c914a295ab3145d61fc3d4b5f
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-07-21 22:14:45 -0500

    modularized environment variable validation

--- python/utils/environment.py
+++ python/utils/environment.py
@@ -1,14 +1,24 @@
 from utils import exceptions, log
 
-def validate(environment, env_vars):
+# dictionary with required environment variables
+env_vars = [
+  "TABLEAU_SERVER",
+  "TABLEAU_SITENAME",
+  "TABLEAU_USERNAME",
+  "TABLEAU_CA_CLIENT",
+  "TABLEAU_CA_SECRET_ID",
+  "TABLEAU_CA_SECRET_VALUE"
+]
+
+def validate(env_dict):
   # check that each environment variable has been declared and assigned
   for vars in env_vars:
     try:
       # check the local dictionary pulled from os.environ
-      environment[vars]
+      env_dict[vars]
 
       # check that key value length is non-zero
-      if len(environment[vars]) == 0:
+      if len(env_dict[vars]) == 0:
         log.logger.critical(f"Environment variable value for key {vars} was not assigned.")
         raise exceptions.EnvironmentError(vars)
 
