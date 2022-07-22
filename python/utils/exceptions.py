504dc00dbfb10aee42fc9cea0e2d0786eabc842c
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-07-21 23:32:34 -0500

    created a JWT exception class for logging and guidance

--- python/utils/exceptions.py
+++ python/utils/exceptions.py
@@ -1,3 +1,5 @@
+from utils import log
+
 class Error(Exception):
   """base class for errors"""
 
@@ -12,5 +14,19 @@ class EnvironmentError(Error):
   def __init__(self, environment_variable):
     self.environment_variable = environment_variable
     self.message = f"Environment variable value for key {environment_variable} was not assigned."
+    super().__init__(self.message)
+    
+class JWTEncodingError(Error):
+  """
+  Exception raised when encoding fails
+
+  Attributes:
+    key_attribute -- environment variable
+    key_attribute -- environment variable
+  """
+
+  def __init__(self, error):
+    self.message = f"Cannot encode JWT: {error}"
+    self.log = log.logger.error(f"cannot encode JWT, {error}")
     super().__init__(self.message)
     
