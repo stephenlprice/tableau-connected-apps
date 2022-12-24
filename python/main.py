89baec320105fe928cd06ed29efdeac709f67e7b
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-12-23 22:53:05 -0600

    importing argparse to add CLI functionality

--- python/main.py
+++ python/main.py
@@ -61,3 +61,19 @@ if __name__ == '__main__':
   # used to output file name, line number and caller when debugging
   def getLineInfo():
     print(inspect.stack()[1][1],":",inspect.stack()[1][2],":", inspect.stack()[1][3])
+
+# CLI
+parser = argparse.ArgumentParser()
+
+parser.add_argument("path")
+
+args = parser.parse_args()
+
+target_dir = Path(args.path)
+
+if not target_dir.exists():
+    print("The target directory doesn't exist")
+    raise SystemExit(1)
+
+for entry in target_dir.iterdir():
+    print(entry.name)
